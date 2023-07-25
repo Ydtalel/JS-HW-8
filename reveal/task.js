c896335825f8a3cb81d50ea2f6e3d0ed566ac84c
Мироненко Юрий <ydtalel@gmail.com>
2023-07-25 11:15:44 +0300

    function isVisible updated

--- reveal/task.js
+++ reveal/task.js
@@ -1,9 +1,11 @@
 function isVisible() {
-  const divElement = document.querySelector('.reveal');
-  const rect = divElement.getBoundingClientRect();
-  const isVisibleOnScreen = rect.top >= 0 && rect.bottom <= window.innerHeight;
-
-  divElement.classList.toggle('reveal_active', isVisibleOnScreen);
+  const divElements = document.querySelectorAll('.reveal');
+  divElements.forEach((el) => {
+    const rect = el.getBoundingClientRect();
+    const isVisibleOnScreen = rect.top >= 0 && rect.bottom <= window.innerHeight;
+    el.classList.toggle('reveal_active', isVisibleOnScreen);
+  });
+  
 }
 
 window.addEventListener('scroll', isVisible);
